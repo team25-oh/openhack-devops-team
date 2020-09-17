b6bf8e155267fc48eefec3bf7894a850e10ed81f
nmishr <nmishr@outlook.com>
2020-09-17 13:28:21 -0400

    Update user.js

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -16,7 +16,7 @@ module.exports = {
         200: function (req, res, callback) {
             res.json({
                 message: 'healthcheck',
-                status:  process.env.DEPLOYMENT_ENVIRONMENT
+                status:  'new_version'
             });
             callback;
         },
